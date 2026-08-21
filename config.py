b8443a593fd7c43b2651552ed258b181c25b1ef9
SteffenOe <steffen@new-zone.de>
2026-08-21 17:51:42 +0200

    add proper env handling

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 class Config:    
     """Konfiguration"""
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-medical-tracker'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASE_DIR, 'database', 'medical_tracker.db')
-    
+    DB_REL_PATH = os.environ.get('DATABASE_PATH', 'database/medical_tracker.db')
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, DB_REL_PATH)}"
